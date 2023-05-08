b3b5b44772f4bac008c4acbc1daf5a6a87a39376
rlharsh <114967560+rlharsh@users.noreply.github.com>
2023-05-08 10:02:46 -0500

    Update

---
+++ Excercises/Brocode/js/4-user-input.js
@@ -0,0 +1,7 @@
+let username;
+
+document.getElementById("myButton").onclick = function() {
+    username = document.getElementById("myText").value;
+    console.log(username);
+    document.getElementById("myLabel").innerHTML = "Hello " + username;
+}
